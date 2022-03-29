c128c1b82828dab8adeaad0f26911092b23d0bb0
aScHEABL <natothun@gmail.com>
2022-03-29 21:53:13 +0800

    feat: add function game()

--- scripts.js
+++ scripts.js
@@ -40,28 +40,36 @@ function playRound (playerSelection) {
 */
 console.log("hello world!")
 
+let computerScores, playerScores;
 function playRound (playerSelection) {
     const options = ["Rock", "Paper", "Scissors"];
+    playerSelection = prompt("Enter \"rock\", \"paper\" or \"scissors\"")
     playerSelection = playerSelection.toUpperCase().slice(0,1) + playerSelection.slice(1).toLowerCase()
     let computerSelection = options[Math.floor(Math.random() * options.length)]
     switch (true) {
         case playerSelection === "Rock" && computerSelection === "Scissors":
             console.log("You win! " + playerSelection + " beats " + computerSelection + " .");
+            playerScores = playerScores++;
             break;
         case playerSelection === "Rock" && computerSelection === "Paper":
             console.log("You lose! " + computerSelection + " beats " + playerSelection + " .");
+            computerScores = computerScores++;
             break;
         case playerSelection === "Paper" && computerSelection === "Rock":
             console.log("You win! " + playerSelection + " beats " + computerSelection + " .");
+            playerScores = playerScores++;
             break;
         case playerSelection === "Paper" && computerSelection === "Scissors":
             console.log("You lose! " + computerSelection + " beats " + playerSelection + " .");
+            computerScores = computerScores++;
             break;
         case playerSelection === "Scissors" && computerSelection === "Paper":
             console.log("You win! " + playerSelection + " beats " + computerSelection + " .");
+            playerScores = playerScores++;
             break;
         case playerSelection  === "Scissors" && computerSelection === "Rock":
             console.log("You lose! " + computerSelection + " beats " + playerSelection + " .");
+            computerScores = computerScores++;
             break;
         case playerSelection === computerSelection:
             console.log("It's a tie!");
@@ -70,6 +78,14 @@ function playRound (playerSelection) {
 
 
 function game() {
-    let computerScores, playerScores
-    
+    for (let i = 0; i < 5; i++) {
+        playRound();
+    }
+    if (playerScores > computerScores) {
+        console.log("Congratulation! You're the winner!");
+    } else if (playerScores < computerScores) {
+        console.log("Too bad! You're the loser!");
+    } else if (playerScores === computerScores) {
+        console.log("Tie! Try again next time!");
+    }
 }
