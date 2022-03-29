ad88efcba4804c6d079a4d3a125fad18ec453a7f
aScHEABL <natothun@gmail.com>
2022-03-29 16:12:04 +0800

    feat: add function "playRound"

---
+++ scripts.js
@@ -0,0 +1,75 @@
+/*
+function computerPlay() {
+    let choice = Math.random() * 3;
+    if (choice <= 1) {
+        choice = "Rock";
+    } else if (choice <= 2) {
+        choice = "Paper";
+    } else {
+        choice = "Scissors";
+    }
+    return choice;
+}
+
+function computerPlay () {
+    const options = ["rock", "paper", "scissors"];
+    return options[Math.floor(Math.random() * options.length)];
+}
+
+
+function playRound (playerSelection) {
+    const options = ["rock", "paper", "scissors"];
+    playerSelection = playerSelection.toLowerCase();
+    let computerSelection = options[Math.floor(Math.random() * options.length)]
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        console.log("You win! " + playerSelection + " beats " + computerSelection + " .");
+    } else if (playerSelection === computerSelection) {
+        console.log("It's a tie!");
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        console.log("You lose! " + computerSelection + " beats " + playerSelection + " .");
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        console.log("You win! " + playerSelection + " beats " + computerSelection + " .");
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        console.log("You lose! " + computerSelection + " beats " + playerSelection + " .");
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        console.log("You win! " + playerSelection + " beats " + computerSelection + " .");
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        console.log("You lose! " + computerSelection + " beats " + playerSelection + " .");
+    }
+}
+*/
+console.log("hello world!")
+
+function playRound (playerSelection) {
+    const options = ["Rock", "Paper", "Scissors"];
+    playerSelection = playerSelection.toUpperCase().slice(0,1) + playerSelection.slice(1).toLowerCase()
+    let computerSelection = options[Math.floor(Math.random() * options.length)]
+    switch (true) {
+        case playerSelection === "Rock" && computerSelection === "Scissors":
+            console.log("You win! " + playerSelection + " beats " + computerSelection + " .");
+            break;
+        case playerSelection === "Rock" && computerSelection === "Paper":
+            console.log("You lose! " + computerSelection + " beats " + playerSelection + " .");
+            break;
+        case playerSelection === "Paper" && computerSelection === "Rock":
+            console.log("You win! " + playerSelection + " beats " + computerSelection + " .");
+            break;
+        case playerSelection === "Paper" && computerSelection === "Scissors":
+            console.log("You lose! " + computerSelection + " beats " + playerSelection + " .");
+            break;
+        case playerSelection === "Scissors" && computerSelection === "Paper":
+            console.log("You win! " + playerSelection + " beats " + computerSelection + " .");
+            break;
+        case playerSelection  === "Scissors" && computerSelection === "Rock":
+            console.log("You lose! " + computerSelection + " beats " + playerSelection + " .");
+            break;
+        case playerSelection === computerSelection:
+            console.log("It's a tie!");
+        }
+}
+
+
+function game() {
+    let computerScores, playerScores
+    
+}
