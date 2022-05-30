76a328ab2af8f7731befbb113fe3ee8433a3bd83
aScHEABL <natothun@gmail.com>
2022-05-30 23:59:47 +0800

    feat: version 3

--- scripts.js
+++ scripts.js
@@ -1,59 +1,47 @@
 console.log(`Hello World!`);
 
-let playerSelection = "";
-let playerScores, computerScores, i = 0;
-const choicesArray = ["rock", "paper", "scissors"];
-const scoresArray = ["playerScores_DOM", "computerScores_DOM"]
-const body_DOM = document.body;
-const displayResult_DOM = document.querySelector(`#displayResult`)
-let scores_DOM = scoresArray.map((scores) => document.querySelector(`#${scores}`));
-let RPS_DOM = choicesArray.map((id) => document.querySelector(`#${id}`));
-
-let computerPlay = () => {
-    console.log(`Computer:${choicesArray[Math.floor(Math.random() * 3)]}`);
-    return choicesArray[Math.floor(Math.random() * 3)];
-};
-
-
-const playGame = () => {
-    computerSelection = computerPlay();
-    if (playerSelection === "rock" && computerSelection === "rock") displayResult_DOM.textContent = "Tie!";
-    else if (playerSelection === "rock" && computerSelection === "scissors") {
-        displayResult_DOM.textContent = "You win this round!";
-        playerScores++;
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-        displayResult_DOM.textContent = "You lose this round!";
-        computerScores++;
-    }
-    let declareWinner = (playerScores === 5) ? displayResult_DOM.textContent = "Congratualation! You're the winner!"
-    : (computerScores === 5) ? displayResult_DOM.textContent = "You lost! Try better next time!"
-}
+let computerSelection,
+  playerSelection = "";
+let computerScores,
+  playerScores = 0;
+const choices = [`rock`, `paper`, `scissors`];
+const RPS_DOM = choices.map((dom_ID) => document.querySelector(`#${dom_ID}`));
 
 for (let loopIteration = 0; loopIteration < 3; loopIteration++) {
-    RPS_DOM[loopIteration].addEventListener("click", () => {
-        playerSelection = choicesArray[loopIteration];
-        playGame();
-        console.log(`Player:${choicesArray[loopIteration]}`);
-    })
+  RPS_DOM[loopIteration].addEventListener(`click`, () => {
+    playerSelection = choices[loopIteration];
+    console.log(`Player:${playerSelection}`);
+    playGame();
+  });
+}
+
+let computerPlay = () => {
+  computerSelection = choices[Math.floor(Math.random() * 3)];
+  console.log(`Computer:${computerSelection}`);
 };
 
-/*
-RPS_DOM[0].addEventListener("click", () => {
-    playerSelection = choicesArray[0];
-    playGame();
-    console.log(`Player:${choicesArray[0]}`);
-});
+let playGame = () => {
+  computerPlay();
+  if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    console.log(`Player won this round!`);
+    playerScores++;
+  } else if (
+    (playerSelection === "rock" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "rock")
+  ) {
+    console.log(`Player lost this round!`);
+    computerScores++;
+  } else console.log(`This round is a tie!`);
 
-RPS_DOM[1].addEventListener("click", () => {
-    playerSelection = choicesArray[1];
-    playGame();
-    console.log(`Player:${choicesArray[1]}`);
-});
-
-RPS_DOM[2].addEventListener("click", () => {
-    playerSelection = RPS_DOM[2];
-    playGame();
-    console.log(`Player:${choicesArray[2]}`);
-});
-*/
-
+  if (playerScores === 5 || computerScores === 5) {
+    let determineWhoWins =
+      playerScores === 5
+        ? console.log(`Player won this game!`)
+        : console.log(`Computer won this game!`);
+  }
+};
