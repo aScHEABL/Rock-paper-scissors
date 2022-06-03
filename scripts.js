b1b9bffc9f1c4e62dfaa553ae16991b869536910
aScHEABL <natothun@gmail.com>
2022-06-03 20:18:00 +0800

    feat: version 6

--- scripts.js
+++ scripts.js
@@ -6,7 +6,8 @@ const choices = [`rock`, `paper`, `scissors`];
 const scores = [`pScores`, `cScores`];
 const RPS_DOM = choices.map((dom_id) => document.querySelector(`#${dom_id}`));
 const scores_DOM = scores.map((score_id) => document.querySelector(`#${score_id}`));
-const result = document.querySelector(`#displayResult`);
+const result_DOM = document.querySelector(`#displayResult`);
+const reset_DOM = document.querySelector(`#reset`);
 
 for (let i = 0; i < choices.length; i++) {
     RPS_DOM[i].addEventListener(`click`, () => {
@@ -28,27 +29,36 @@ let playGame = () => {
     console.log(`Computer scores:${computerScores}`);
 
     if (playerScores === 5 || computerScores === 5) {
+        for (let i = 0; i < RPS_DOM.length; i++) RPS_DOM[i].disabled = true;
+        resetGame();
         let checkWinner =
         playerScores === 5
-        ? result.textContent = `Player has won this game!`
-        : result.textContent = `Computer has won this game!`;
+        ? result_DOM.textContent = `Player has won this game!`
+        : result_DOM.textContent = `Computer has won this game!`;
     }
 };
 
 let compareChoices = (player, computer) => {
     switch (((player - computer) - 1) % 3) {
         case 0:
-            result.textContent = `Result:${choices[playerIndexOfChoices]} vs ${choices[computerIndexOfChoices]}. You won this round!`
+            result_DOM.textContent = `Result:${choices[playerIndexOfChoices]} vs ${choices[computerIndexOfChoices]}. You won this round!`
             playerScores++;
             pScores.textContent = `Player Scores:${playerScores}`;
             break;
         case -2:
-            result.textContent = `Result:${choices[playerIndexOfChoices]} vs ${choices[computerIndexOfChoices]}. You lost this round!`
+            result_DOM.textContent = `Result:${choices[playerIndexOfChoices]} vs ${choices[computerIndexOfChoices]}. You lost this round!`
             computerScores++;
             cScores.textContent = `Computer Scores:${computerScores}`;
             break;
         case -1:
-            result.textContent = `Result:It's a tie this round!`
+            result_DOM.textContent = `Result:${choices[playerIndexOfChoices]} vs ${choices[computerIndexOfChoices]}. It's a tie this round!`
             break;
     };
 };
+
+let resetGame = () => {
+    reset_DOM.disabled = false;
+    reset_DOM.addEventListener(`click`, () => {
+        location.reload();
+    })
+}
